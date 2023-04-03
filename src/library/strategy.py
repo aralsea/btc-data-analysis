7e9d47ac4fa42525c1828e393f07b698cf2d9f5f
shtmjp <shtm.1058@gmail.com>
2023-04-03 22:41:25 +0900

    Strategy classの例を実装

--- src/library/strategy.py
+++ src/library/strategy.py
@@ -1,24 +1,32 @@
+from typing import NamedTuple
+
 import pandas as pd
 import pandas_ta as ta
 
-from library.simulator import MarketOrder, Order
+from library.simulator import BackTester, MarketOrder, Order
 
 
-class AbstractStrategy:
-    def __init__(self, df):
+class OrderwithTime(NamedTuple):
+    order: Order
+    orderingtime: pd.Timestamp
+
+
+class TestStrategy:
+    def __init__(self, df: pd.DataFrame):
         # dfのindexは"timestamp", columnsは"open", "high", "low", "close", "volume"
         self.df = df
+        self.orders_to_be_added: list[OrderwithTime] = []
 
-    # 現在時刻までの情報を受け取り、注文を出力する関数
-    def make_order(
+    # 現在時刻までの情報を受け取り、注文をself.orders_to_be_addedに追加
+    def make_order_gc(
         self,
         now_time: pd.Timestamp,
         now_cash: float,
         now_position: float,
         active_orders: list[Order],
-    ) -> list[Order]:
+    ) -> None:
         # 現在時刻までのデータを使って注文を出す
-        tmp_df = self.df.loc[:now_time]
+        tmp_df = self.df[self.df.index <= now_time]
         now_price = tmp_df["close"].iloc[-1]
         expected_max_size = now_cash / now_price
         size = expected_max_size * 0.1
@@ -30,13 +38,30 @@ class AbstractStrategy:
             sma_20 = ta.sma(close=tmp_df["close"], length=20).values
             if sma_10[-1] > sma_20[-1] and sma_10[-2] < sma_20[-2]:
                 # 買い注文
-                order1 = MarketOrder(timestamp=now_time, side="BUY", size=size)
-                # 逆の売り注文を1期後に出し、ポジション解消したい
-                # TODO
-                # order2 = MarketOrder(
-                #   timestamp=now_time + pd.Timedelta(min=15), side="SELL", size=size
-                # )
-                # のようにしたい
-        return [
-            order1,
-        ]
+                o1 = OrderwithTime(
+                    order=MarketOrder(timestamp=now_time, side="BUY", size=size),
+                    orderingtime=now_time,
+                )
+                self.orders_to_be_added.append(o1)
+                # 1期後にポジション解消
+                # FIXME: 実際にはslippageの影響でsizeが少し小さくなる
+                o2 = OrderwithTime(
+                    order=MarketOrder(timestamp=now_time, side="SELL", size=size),
+                    orderingtime=now_time + pd.Timedelta(seconds=900),
+                )
+                self.orders_to_be_added.append(o2)
+
+    def add_order(self, now_time: pd.Timestamp, tester: BackTester) -> None:
+        # now_timeまでに発注すべき注文をtesterに渡す
+        remained_orders_to_be_added = []
+        for o in self.orders_to_be_added:
+            if o.orderingtime <= now_time:
+                tester.add_order(o.order)
+            else:
+                remained_orders_to_be_added.append(o)
+        self.orders_to_be_added = remained_orders_to_be_added
+
+
+class AbstractStrategy:
+    def __init__(self, df):
+        return None
