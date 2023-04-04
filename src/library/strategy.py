3b53ae056400a8541e834f8d4e44f5ab2279a4e3
shtmjp <shtm.1058@gmail.com>
2023-04-04 13:33:37 +0900

    bug

--- src/library/strategy.py
+++ src/library/strategy.py
@@ -18,9 +18,10 @@ class TestStrategy:
         self.orders_to_be_added: list[OrderwithTime] = []
 
     # 現在時刻までの情報を受け取り、注文をself.orders_to_be_addedに追加
-    def make_order_gc(
+    def make_order(
         self,
         now_time: pd.Timestamp,
+        now_price: float,
         now_cash: float,
         now_position: float,
         active_orders: list[Order],
@@ -29,7 +30,7 @@ class TestStrategy:
         tmp_df = self.df[self.df.index <= now_time]
         now_price = tmp_df["close"].iloc[-1]
         expected_max_size = now_cash / now_price
-        size = expected_max_size * 0.1
+        size = expected_max_size * 0.01
 
         # ゴールデンクロス
         # 10期SMAが20期SMAを上回った瞬間に買い
@@ -46,13 +47,18 @@ class TestStrategy:
                 # 1期後にポジション解消
                 # FIXME: 実際にはslippageの影響でsizeが少し小さくなる
                 o2 = OrderwithTime(
-                    order=MarketOrder(timestamp=now_time, side="SELL", size=size),
-                    orderingtime=now_time + pd.Timedelta(seconds=900),
+                    order=MarketOrder(
+                        timestamp=now_time + pd.Timedelta(seconds=900 * 5),
+                        side="SELL",
+                        size=size,
+                    ),
+                    orderingtime=now_time + pd.Timedelta(seconds=900 * 5),
                 )
                 self.orders_to_be_added.append(o2)
 
     def add_order(self, now_time: pd.Timestamp, tester: BackTester) -> None:
         # now_timeまでに発注すべき注文をtesterに渡す
+        # now_timeにtesterに渡した注文は, now_time + 15minに実行される
         remained_orders_to_be_added = []
         for o in self.orders_to_be_added:
             if o.orderingtime <= now_time:
@@ -60,6 +66,31 @@ class TestStrategy:
             else:
                 remained_orders_to_be_added.append(o)
         self.orders_to_be_added = remained_orders_to_be_added
+
+
+class RunnerwithStrategy:
+    def __init__(self, tester: BackTester, strategy) -> None:
+        self.tester = tester
+        self.strategy = strategy
+
+    def run(self) -> None:
+        # ランダムに売買する
+        for i, now_time in enumerate(self.tester):
+            # 現在時刻までの情報を取得
+            now_price = self.tester.tick.close
+            now_cash = self.tester.cash
+            now_position = self.tester.position
+            active_orders = self.tester.active_orders
+            # 注文を作成
+            self.strategy.make_order(
+                now_time=now_time,
+                now_price=now_price,
+                now_cash=now_cash,
+                now_position=now_position,
+                active_orders=active_orders,
+            )
+            # testerに注文を渡す
+            self.strategy.add_order(now_time=now_time, tester=self.tester)
 
 
 class AbstractStrategy:
