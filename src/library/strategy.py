e5497fda5b1daeeec14481c0ac0078b35c305b7b
shtmjp <shtm.1058@gmail.com>
2023-04-04 13:37:10 +0900

    ゴールデンクロスの動作確認

--- src/library/strategy.py
+++ src/library/strategy.py
@@ -1,21 +1,14 @@
-from typing import NamedTuple
-
 import pandas as pd
 import pandas_ta as ta
 
 from library.simulator import BackTester, MarketOrder, Order
 
 
-class OrderwithTime(NamedTuple):
-    order: Order
-    orderingtime: pd.Timestamp
-
-
 class TestStrategy:
     def __init__(self, df: pd.DataFrame):
         # dfのindexは"timestamp", columnsは"open", "high", "low", "close", "volume"
         self.df = df
-        self.orders_to_be_added: list[OrderwithTime] = []
+        self.orders_to_be_added: list[Order] = []
 
     # 現在時刻までの情報を受け取り、注文をself.orders_to_be_addedに追加
     def make_order(
@@ -39,30 +32,23 @@ class TestStrategy:
             sma_20 = ta.sma(close=tmp_df["close"], length=20).values
             if sma_10[-1] > sma_20[-1] and sma_10[-2] < sma_20[-2]:
                 # 買い注文
-                o1 = OrderwithTime(
-                    order=MarketOrder(timestamp=now_time, side="BUY", size=size),
-                    orderingtime=now_time,
-                )
+                o1 = MarketOrder(timestamp=now_time, side="BUY", size=size)
                 self.orders_to_be_added.append(o1)
-                # 1期後にポジション解消
+                # 5期後にポジション解消
                 # FIXME: 実際にはslippageの影響でsizeが少し小さくなる
-                o2 = OrderwithTime(
-                    order=MarketOrder(
-                        timestamp=now_time + pd.Timedelta(seconds=900 * 5),
-                        side="SELL",
-                        size=size,
-                    ),
-                    orderingtime=now_time + pd.Timedelta(seconds=900 * 5),
+                o2 = MarketOrder(
+                    timestamp=now_time + pd.Timedelta(seconds=900 * 5),
+                    side="SELL",
+                    size=size,
                 )
                 self.orders_to_be_added.append(o2)
 
     def add_order(self, now_time: pd.Timestamp, tester: BackTester) -> None:
         # now_timeまでに発注すべき注文をtesterに渡す
-        # now_timeにtesterに渡した注文は, now_time + 15minに実行される
         remained_orders_to_be_added = []
         for o in self.orders_to_be_added:
-            if o.orderingtime <= now_time:
-                tester.add_order(o.order)
+            if o.timestamp <= now_time:
+                tester.add_order(o)
             else:
                 remained_orders_to_be_added.append(o)
         self.orders_to_be_added = remained_orders_to_be_added
